97956abbab2fed602cc18d5086009ec6760f8240
wannabeDevv <shalupadam@gmail.com>
2025-01-11 10:56:39 +0530

    Eyes

--- ochi/src/App.jsx
+++ ochi/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import LandingPage from './components/landingpage'
 import Marquee from './components/Marquee'
 import About from './components/About'
+import Eyes from './components/Eyes'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <LandingPage/>
     <Marquee/>
     <About/>
+    <Eyes/>
     </div>
   )
 }
